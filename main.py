555d83ab494c9db495c45ef5e1c7153c98c5e391
Paulo H A Silva <paulohenriqueas006@gmail.com>
2024-10-06 16:33:19 -0300

    recebendo os preços e salvando uma copia do html

--- main.py
+++ main.py
@@ -9,22 +9,24 @@ from bs4 import BeautifulSoup
 class Product:
     def __init__(self, name):
         self.name = name
+    def __str__(self):
+        return f"{self.name}"
 
 # Função de busca
 
-def prodSearch(name):
-    name = name.replace(' ','+')
-    req = requests.get('https://www.kabum.com.br/busca?query={}'.format(name))
+def prodSearch(name, store):
+    if store.lower() == 'kabum':
+        req = requests.get('https://www.kabum.com.br/busca/{}'.format(name.replace(' ','%20')))
     soup = BeautifulSoup(req.text, 'html.parser')
-    print(soup)
+    with open('soup.html', "w", encoding="utf-8") as f:
+        f.write(req.text)
+    all_tag_span = soup.find_all('span')
+    for tag_span in all_tag_span:
+        class_span = tag_span.get('class')
+        if 'sc-' in str(class_span):
+            print(tag_span.text)
 
-
-
-
+############ MAIN
 name = input('Hardware a ser buscado:')
-
 product = Product(name)
-
-
-
-
+prodSearch(product.name, 'kabum')
